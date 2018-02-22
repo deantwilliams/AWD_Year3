623664cda15ceaa418e07bc697cfc6a852876b65
shahzadr321 <shahzadr321@gmail.com>
2018-02-22 11:51:03 +0000

    counter
    counter

--- client/js/controllers/counterController.js
+++ client/js/controllers/counterController.js
@@ -1,7 +1,30 @@
 angular.module('myApp').controller('counterController', [ '$routeParams', '$location', '$route', '$scope', 'ItemService', 'OrderService', function( $routeParams, $location, $route, $scope, ItemService, OrderService ){
 
 	$scope.title = "Counter"
-
-
-
+    $scope.tableOrders = [];
+	
+  OrderService.getOrders().then ( function( allOrders ){
+	$scope.allOrders = allOrders  
+	$scope.tableOrdersShow = true;
+  })
+  
+	$scope.selectTable = function( tableNumber ) {
+		for(var i=0;i<$scope.allOrders.length;i++)
+		{
+			if($scope.allOrders[i].tableNumber == tableNumber)
+			{
+				$scope.tableOrders.push($scope.allOrders[i]);
+				console.log($scope.tableOrders);
+			}
+		}
+		
+	}
+	$scope.totalPrice = function(){
+		var total=0;
+		for (count=0;count<$scope.tableOrders.length;count++){
+		total += $scope.item[count].price +$scope.item[count].price;
+		}
+		return total;
+	}
+$scope
 }]);
