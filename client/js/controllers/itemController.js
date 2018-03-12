901153f7e921e30352d4e927b4dc9e21ea941815
Alistair Dickson <11025906@stu.mmu.ac.uk>
2018-03-12 16:17:40 +0000

    fixed item edit

--- client/js/controllers/itemController.js
+++ client/js/controllers/itemController.js
@@ -1,14 +1,15 @@
 angular.module('myApp').controller('itemController', function( $scope, $routeParams, $location,  ItemService, UserService ){
 
+	ItemService.getItems().then( function( allItems ){
+		$scope.allItems = allItems
+	})
+
  	var itemId = $routeParams.id;
 
-    $scope.error = {}
 
     if( itemId ){
 
       ItemService.getItem( itemId ).then( function( item ){
-
-      	console.log(JSON.stringify(item, null, 2))
 
         $scope.item = item;
 
@@ -20,6 +21,16 @@ angular.module('myApp').controller('itemController', function( $scope, $routePar
 
     $scope.updateItem = function(){
 
+	// Check whether an item with the same id exists.
+		// If it exists, an new menu item cannot be added 
+		var itemWithId = $scope.allItems.findIndex(x => x.id == $scope.item.id);
+		
+		console.log("id: " + itemId);
+		
+		if(itemWithId != -1){
+			// Item with provided id already exists 			
+           $scope.errorMessage = "This ID is already in use, please use a different one.";
+		}else{
 
 		ItemService.updateItem( $scope.item ).then( function(){
 
@@ -28,7 +39,7 @@ angular.module('myApp').controller('itemController', function( $scope, $routePar
 		}, function(){
 
 		})
-
+		}
     }
 
 })
